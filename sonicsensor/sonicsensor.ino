486dfc1d8ef9962dffa2d3f866bdb629bba8e688
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2023-03-22 08:52:35 +0100

    functie sonicsensor nog niet klaar

---
+++ sonicsensor/sonicsensor.ino
@@ -0,0 +1,32 @@
+const int TRIG_PIN = 6; // Arduino pin connected to Ultrasonic Sensor's TRIG pin
+const int ECHO_PIN = 7; // Arduino pin connected to Ultrasonic Sensor's ECHO pin
+
+float duration_us, distance_cm; // variable 
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600);       // initialize serial port
+  pinMode(TRIG_PIN, OUTPUT); // set arduino pin to output mode
+  pinMode(ECHO_PIN, INPUT);  // set arduino pin to input mode
+  pinMode(LED_PIN, OUTPUT);  // set arduino pin to output mode
+}
+
+void pingsensor(){
+  // give 10 microseconds pulse on trig pin
+   digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+  // calculate the distance
+  distance_cm = 0.017 * duration_us;
+  if(distance_cm < 5)
+  
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
