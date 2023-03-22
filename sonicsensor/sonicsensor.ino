0820af22848388726f1786bb2c3973e120a7ffa1
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2023-03-22 09:18:21 +0100

    functie ping sensor toegevoegt

--- sonicsensor/sonicsensor.ino
+++ sonicsensor/sonicsensor.ino
@@ -1,32 +1,30 @@
-const int TRIG_PIN = 6; // Arduino pin connected to Ultrasonic Sensor's TRIG pin
-const int ECHO_PIN = 7; // Arduino pin connected to Ultrasonic Sensor's ECHO pin
+//pingsensor
+const int TRIG_PIN = A4;
+const int ECHO_PIN = A5;
 
-float duration_us, distance_cm; // variable 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin (9600);       // initialize serial port
-  pinMode(TRIG_PIN, OUTPUT); // set arduino pin to output mode
-  pinMode(ECHO_PIN, INPUT);  // set arduino pin to input mode
-  pinMode(LED_PIN, OUTPUT);  // set arduino pin to output mode
+  // serial start
+  Serial.begin(9600);
+  // setup ping sensor
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
-void pingsensor(){
-  // give 10 microseconds pulse on trig pin
-   digitalWrite(TRIG_PIN, HIGH);
+void pingsensor() {
+  // give 10 microseconds pulse on trig pin to send a sound wave
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-
   // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(ECHO_PIN, HIGH);
+  float duration_us = pulseIn(ECHO_PIN, HIGH);
   // calculate the distance
-  distance_cm = 0.017 * duration_us;
-  if(distance_cm < 5)
-  
-
-
+  float distance_cm = 0.017 * duration_us;
+  if (distance_cm < 10) {}
+  Serial.print("distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  pingsensor();
 }
