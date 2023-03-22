62f4d708343355e218fb36f84f68fe0e43f465f5
Simon van den Berg <simonschrotenberg@gmail.com>
2023-03-22 09:58:44 +0100

    added starting function

--- sonicsensor/sonicsensor.ino
+++ sonicsensor/sonicsensor.ino
@@ -1,22 +1,22 @@
 //pingsensor
-const int TRIG_PIN = A4;
-const int ECHO_PIN = A5;
+const int TrigPin = A4;
+const int EchoPin = A5;
 
 void setup() {
   // serial start
   Serial.begin(9600);
   // setup ping sensor
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  pinMode(TrigPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
 }
 
 void pingsensor() {
   // give 10 microseconds pulse on trig pin to send a sound wave
-  digitalWrite(TRIG_PIN, HIGH);
+  digitalWrite(TrigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+  digitalWrite(TrigPin, LOW);
   // measure duration of pulse from ECHO pin
-  float duration_us = pulseIn(ECHO_PIN, HIGH);
+  float duration_us = pulseIn(EchoPin, HIGH);
   // calculate the distance
   float distance_cm = 0.017 * duration_us;
   if (distance_cm < 10) {}
